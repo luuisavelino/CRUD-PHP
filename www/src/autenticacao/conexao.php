2a0debea65eb19835f572019f84032dd7b321865
unknown <luuis.avelino@gmail.com>
2022-01-21 14:54:10 -0300

    correção de senhas

--- www/src/autenticacao/conexao.php
+++ www/src/autenticacao/conexao.php
@@ -1,7 +1,7 @@
 <?php
-define('HOST', 'localhost');
+define('HOST', 'mysql');
 define('USUARIO', 'root');
-define('SENHA', 'mariadb@password!');
+define('SENHA', 'admin');
 define('DB', 'challenge');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
